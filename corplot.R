a24182f1b5b4a127419fb325a19f152f62e1f11e
Paco Cruz <paco.cruz@gmail.com>
2017-12-19 08:03:14 -0800

    R script generates png files directly

--- corplot.R
+++ corplot.R
@@ -1,5 +1,5 @@
 # Get data
-install.packages('corrplot')
+# install.packages('corrplot')
 library(corrplot)
 library(readr)
 datawork <- read_csv("./data_out/all_features_with_sentiment_merge.csv", 
@@ -10,5 +10,9 @@ datawork <- read_csv("./data_out/all_features_with_sentiment_merge.csv",
                                       reviewLength = col_double(), reviewText = col_skip(), 
                                       reviewWords = col_double(), sentiment = col_double(), 
                                       usefulness = col_double()))
+png('./data_out/corplotR.png')
 corrplot.mixed(cor(datawork))
+dev.off()
+png('./data_out/corplotValuesR.png')
 corrplot(cor(datawork), method = 'number')
+dev.off()
